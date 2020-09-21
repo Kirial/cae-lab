5ca4bc5921ea7cd490222938edfbada186bf72f2
Kirial <jesperkirial@gmail.com>
2020-09-21 11:01:20 +0200

    added folder for report 1

--- lab2/hello.s
+++ lab2/hello.s
@@ -2,10 +2,25 @@
 # Print Hello World
 #
 
+main:
 .text
 la a1, hello
 li a0, 4
 ecall
 .data
+
 hello:
-.asciiz "Hello World"
+    .asciiz "Hello World"
+
+
+
+#loop:
+
+
+#li x19 0x10000000 //Load 
+#addi x19 x19 1
+
+#blt a2 x0 exit //break less than 0
+
+
+#exit:
